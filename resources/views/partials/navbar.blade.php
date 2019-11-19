b6160b4fbcf264f2f63e228058e606061c1b3478
Miguel <mike17798@gmail.com>
2019-11-19 19:59:19 +0100

    Esqueleto y primeras vistas

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="/" style="color:#777"><span style="font-size:15pt">&#9820;</span> Lista de la compra Laravel</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        @if( true || Auth::check() )
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item {{ Request::is('catalog') && ! Request::is('catalog/create')? 'active' : ''}}">
+                        <a class="nav-link" href="{{url('/productos')}}">
+                            <span class="glyphicon glyphicon-film" aria-hidden="true"></span>
+                            Catálogo Productos
+                        </a>
+                    </li>
+                    <li class="nav-item {{  Request::is('catalog/create') ? 'active' : ''}}">
+                        <a class="nav-link" href="{{url('/productos/create')}}">
+                            <span>&#10010</span> Nuevo articulo
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav navbar-right">
+                    <li class="nav-item">
+                        <form action="{{ url('/logout') }}" method="GET" style="display:inline">
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-link nav-link" style="display:inline;cursor:pointer">
+                                Cerrar sesión
+                            </button>
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        @endif
+    </div>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('login', function () {
+    return view("auth.login");
+});
+Route::get('logout', function () {
+    return  "logout";
+});
+Route::get('productos', function () {
+    return view("productos.index");
+});
+Route::get('productos/show/{id}', function ($id) {
+    return  view("productos.show" , array('id'=>$id));
+})->where('id','[0-9]+');
+Route::get('productos/create', function () {
+    return view("productos.create");
+});
+Route::get('productos/edit/{id}', function ($id) {
+    return view("productos.edit" , array('id'=>$id));
+})->where('id', '[0-9]+');
