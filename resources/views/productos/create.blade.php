200157c9fc9c83d5627b3e29381504c6de3cb2b4
Miguel <mike17798@gmail.com>
2019-11-20 14:03:15 +0100

    Ejercicios Controladores filtros y formularios

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -2,6 +2,54 @@
 
 @section('content')
 
-    Crear Producto
+<div class="row" style="margin-top:40px">
+   <div class="offset-md-3 col-md-6">
+      <div class="card">
+         <div class="card-header text-center">
+            Añadir producto
+         </div>
+         <div class="card-body" style="padding:30px">
+
+            <form action="#" method="post" enctype="multipart/form-data">
+
+            @csrf
+
+            <div class="form-group">
+               <label for="nombre">Nombre</label>
+               <input type="text" name="nombre" id="nombre" class="form-control">
+            </div>
+
+            <div class="form-group">
+              <label for="precio">Precio</label>
+              <input type="number" name="precio" id="precio" class="form-control">
+            </div>
+
+            <div class="form-group">
+              <label for="categoria">Categoría</label>
+              <input type="text" name="categoria" id="categoria" class="form-control">
+            </div>
+
+            <div class="form-group">
+              <label for="imagen">Imagen</label>
+              <input type="text" name="imagen" id="imagen" class="form-control">
+            </div>
+
+            <div class="form-group">
+               <label for="descripcion">Descripción</label>
+               <textarea name="descripcion" id="descripcion" class="form-control" rows="3"></textarea>
+            </div>
+
+            <div class="form-group text-center">
+               <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                   Añadir producto
+               </button>
+            </div>
+
+          </form>
+
+         </div>
+      </div>
+   </div>
+</div>
 
 @stop
--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-    Detalle Producto {{ $id }}
+    Detalle Producto {{ $arrayProductos[0] }}
+    <div class="row">
+
+        <div class="col-sm-4">
+
+            <img src="https://picsum.photos/200/300/?random" style="height:200px"/>
+
+        </div>
+        <div class="col-sm-8">
+
+            <h1>{{$arrayProductos[0]}}</h1>
+            <h2>Categoria :{{$arrayProductos[1]}}</h2>
+            <h3>Estado: Producto actualmente comprado</h3>
+            <button type="button" class="btn btn-danger">Pendiente de compra</button>
+            <button type="button" class="btn btn-warning">Editar Producto</button>
+            <button type="button" class="btn btn-default">Volver al listado</button>
+        </div>
+    </div>
 
 @stop
